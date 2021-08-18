841889a701e7fdad664b6eefa2fd3d7ace2ef38f
Andres Felipe Cabezas Casas <andres.cabezas00@gmail.com>
2021-08-17 21:14:01 -0500

    first commit

---
+++ PruebaTecnicaEfecty/Models/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PruebaTecnicaEfecty.Models
+{
+    public partial class Persona
+    {
+        public int RowId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string TipoDocumento { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public decimal? ValorGanar { get; set; }
+        public string EstadoCivil { get; set; }
+    }
+}
---
+++ PruebaTecnicaEfecty/Models/PruebaTecnicaContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PruebaTecnicaEfecty.Models
+{
+    public partial class PruebaTecnicaContext : DbContext
+    {
+        public PruebaTecnicaContext()
+        {
+        }
+
+        public PruebaTecnicaContext(DbContextOptions<PruebaTecnicaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=ANDRES-PC\\SQLEXPRESS;Database=PruebaTecnica;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
+
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.HasKey(e => e.RowId)
+                    .HasName("PK_Table_1");
+
+                entity.ToTable("Persona");
+
+                entity.Property(e => e.RowId).HasColumnName("rowId");
+
+                entity.Property(e => e.Apellido).HasMaxLength(50);
+
+                entity.Property(e => e.EstadoCivil)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.FechaNacimiento).HasColumnType("datetime");
+
+                entity.Property(e => e.Nombre).HasMaxLength(50);
+
+                entity.Property(e => e.TipoDocumento)
+                    .HasMaxLength(10)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.ValorGanar).HasColumnType("decimal(28, 4)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
